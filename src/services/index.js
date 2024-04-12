a59516cabefd5d69291e521adbf04fcf73dccae8
Nanna <nanaa.gonzalez@gmail.com>
2024-04-13 00:11:15 +0200

    Fix vercel can read .json archives

--- src/services/index.js
+++ src/services/index.js
@@ -1,40 +1,40 @@
 export const getProductsService = async () => {
-  const data = await fetch('./src/mockups/Inventory.json')
+  const data = await fetch('/mockups/Inventory.json')
   const products = await data.json()
 
   return products
 }
 
 export const getSuppliersService = async () => {
-  const data = await fetch('./src/mockups/Suppliers.json')
+  const data = await fetch('/mockups/Suppliers.json')
   const suppliers = await data.json()
 
   return suppliers
 }
 
 export const getClientsService = async () => {
-  const data = await fetch('./src/mockups/Clients.json')
+  const data = await fetch('/mockups/Clients.json')
   const clients = await data.json()
 
   return clients
 }
 
 export const getSalesService = async () => {
-  const data = await fetch('./src/mockups/Sales.json')
+  const data = await fetch('/mockups/Sales.json')
   const sales = await data.json()
 
   return sales
 }
 
 export const getReceiptsService = async () => {
-  const data = await fetch('./src/mockups/Receipt.json')
+  const data = await fetch('/mockups/Receipt.json')
   const receipts = await data.json()
 
   return receipts
 }
 
 export const getOrdersService = async () => {
-  const data = await fetch('./src/mockups/Orders.json')
+  const data = await fetch('/mockups/Orders.json')
   const orders = await data.json()
 
   return orders
